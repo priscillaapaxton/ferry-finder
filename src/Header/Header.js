0d187423b763316c49fc9fa0a949266598801d90
Priscilla Paxton <ppaxton22@turing.edu>
2023-06-07 13:47:41 -0600

    This is ammended commit from erroneous commit before

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,11 @@
 import './Header.css'
+import { Link, NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({resetResults}) => {
   return(
     <header className='header'>
       <div className='header-container'>
+        <Link to='/' onClick={resetResults}>RETURN HOME</Link>
         <h1 className='header-text'>Ferry Finder!</h1>
         <p className='header-text'>We've got you.</p>
       </div>
